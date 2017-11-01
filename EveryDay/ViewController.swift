35a00496e2dac8b9efbf4149a0d890a62e36ebfb
wansheng <wansheng_ios@sina.com>
2017-11-01 17:43:41 +0800

    project init

---
+++ EveryDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EveryDay
+//
+//  Created by 万圣 on 2017/11/1.
+//  Copyright © 2017年 万圣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
